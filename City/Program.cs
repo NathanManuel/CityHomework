7d934c332dade80284716af82dc349181d2beb53
Nathan van Velden <nathanvanvelden@gmail.com>
2021-02-26 17:49:38 +0200

    Deleted cars cuz i dont know how to do it with more files so i am doing it all in the main file

--- City/Program.cs
+++ City/Program.cs
@@ -2,6 +2,11 @@
 
 namespace City
 {
+    public class Person
+    {
+        public int id;
+        public string name = null;
+    }
     class Program
     {
         static void Main(string[] args)
