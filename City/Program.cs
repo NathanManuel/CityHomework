a26d1bd3e32fa70aa59937e7e608f384f4f21637
Nathan van Velden <nathanvanvelden@gmail.com>
2021-02-26 18:02:52 +0200

    Added basic car

--- City/Program.cs
+++ City/Program.cs
@@ -2,16 +2,42 @@
 
 namespace City
 {
-    public class Person
+    public class Cars
     {
-        public int id;
-        public string name = null;
+        private string carName = null;
+        private int carNumber;
+        private string v1;
+        private int v2;
+
+      
+
+        public Cars(string carName, int carNumber)
+        {
+            this.carName = carName;
+            this.carNumber = carNumber;
+        }
+
+        override
+    public string ToString()
+        {
+            string text;
+            text ="The cars name is " + this.carName + " and has the number plate " + this.carNumber;
+            return text;
+        }
+
+        
     }
+   
     class Program
     {
         static void Main(string[] args)
         {
+            Cars c = new Cars("BWM", 123);
+
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(c);
+
         }
     }
 }
